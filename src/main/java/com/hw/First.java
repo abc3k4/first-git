1e99f502a6b2477799aca3539c80c8aeb56c2fe4
fql <1253248392@qq.com>
2019-09-18 16:53:23 +0800

    2.0

---
+++ src/main/java/com/hw/First.java
@@ -0,0 +1,12 @@
+package com.hw;
+
+/**
+ * @aurhor:fql
+ * @date 2019/9/18 16:52
+ * @type:
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World git");
+    }
+}
